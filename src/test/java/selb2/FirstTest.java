b88b2192772792f77870bc4de2d3b7c56174fcbf
Kroz666 <47473500+Kroz666@users.noreply.github.com>
2019-02-10 01:10:56 -0800

    Lib changed
    Changed library.

--- src/test/java/selb2/FirstTest.java
+++ src/test/java/selb2/FirstTest.java
@@ -1,20 +1,20 @@
 package selb2;
 
-
 import org.junit.BeforeClass;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
     public static WebDriver driver;
-            @BeforeClass
-    public static void setup () {
+
+    public FirstTest() {
+    }
+
+    @BeforeClass
+    public static void setup() {
         System.setProperty("webdriver.chrome.driver", "C:\\Webdrivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://mail.ru/");
-            }
-
-
+    }
 }
